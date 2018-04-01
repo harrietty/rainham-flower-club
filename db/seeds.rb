6ebe4688b6ce80a9fe5ef58a6d9be820c71f7ec5
Harriet Ryder <harriethryder@gmail.com>
2018-04-01 16:38:54 +0100

    Add gallery photos and seed galleries and images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Delete all record first to avoid duplication
+
+Gallery.delete_all
+Image.delete_all
+
+hearts_and_flowers = Gallery.create(title: 'Hearts and Flowers: Feb 2017')
+area_show = Gallery.create(title: 'Area Show, May 2016')
+national_flower_day = Gallery.create(title: 'National Flower Arranging Day, May 2016')
+dem_and_dine = Gallery.create(title: 'Dem and Dine: Nov 2015')
+
+# For each gallery, create records for the associated images
+
+['day1', 'day2', 'day3'].each do |img|
+  national_flower_day.images << Image.new(path: "gallery_photos/#{img}.jpg")
+end
+
+['dd1', 'dd2', 'dd3', 'dd4', 'dd5', 'dd6', 'dd7'].each do |img|
+  dem_and_dine.images << Image.new(path: "gallery_photos/#{img}.jpg")
+end
+
+['haf1', 'haf2', 'haf3', 'haf4'].each do |img|
+  hearts_and_flowers.images << Image.new(path: "gallery_photos/#{img}.jpg")
+end
+
+['show1', 'show2', 'show3', 'show4', 'show5', 'show6', 'show7'].each do |img|
+  area_show.images << Image.new(path: "gallery_photos/#{img}.jpg")
+end
